0e01b66b4ee938b4e9edfe01489f7f91e82ebeb8
Nouf Aljowaysir <na1215@nyu.edu>
2017-12-06 02:58:02 -0500

    first commit

---
+++ memories.js
@@ -0,0 +1,66 @@
+
+ var data = {
+ 	"0": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/snow.jpg",
+			"colour": 0x3572B0
+	},
+
+	"1": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/woodshop.jpg",
+			"colour": 0x3BAB60
+	},
+
+	"2": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/museum.jpg",
+			"colour": 0x999999
+	},
+
+	"3": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/working.jpg",
+			"colour": 0x303030
+	},
+
+	"4": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/alaska.jpg",
+			"colour": 0x091F38
+	},
+
+	"5": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/market.jpg",
+			"colour": 0xC2CEDC
+	},
+
+	"6": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/forest.jpg",
+			"colour": 0xFAFAFA
+	},
+
+	"7": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/history.jpg",
+			"colour": 0xF98400
+	},
+
+	"8": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/room.jpg",
+			"colour": 0xD9FF26
+	},
+
+	"9": {
+			"text": "You have selected a memory. Do you wish to enter this memory or keep exploring?",
+			"image": "img/rooftop.jpg",
+			"colour": 0xFF213D
+	}
+
+}
+
+
+	
