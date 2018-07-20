90d926afe86103754d60e339347c39f3b3486ee5
Nouf Aljowaysir <na1215@nyu.edu>
2018-07-19 23:42:04 -0400

    Update memory.js

--- memory.js
+++ memory.js
@@ -23,17 +23,16 @@ let INTERSECTED2;
 let intersects;
 let raycaster = new THREE.Raycaster();
 let arrow;
-//var z;
 let isBallClicked = false;
 
-// VR functionality
+// vr functionality
 let effect;
 let vrManager;
 let vrDisplay;
 let vrButton;
 let vrFrame;
 
-// Physics variables
+// physics variables
 let collisionConfiguration;
 let dispatcher;
 let broadphase;
@@ -44,6 +43,7 @@ let transformAux1 = new Ammo.btTransform();
 let time = 0;
 let cameraSphere;
 
+// water effect
 let frontRefractor;
 let leftRefractor;
 let rightRefractor;
@@ -51,50 +51,57 @@ let bottomRefractor;
 let backRefractor;
 let topRefractor;
 
+//perform three.js initialization scene when window finishes loading
 window.addEventListener('load', onLoad);
 container = document.getElementById('container');
 
 function onLoad() {
-  //loadFiles(data);
   clock = new THREE.Clock();
   let wid = window.innerWidth;
   let hei = window.innerHeight;
   let container = document.getElementById('container');
 
-  // INITIALIZATION
+  // initialization
   renderer = new THREE.WebGLRenderer({});
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(wid, hei);
   container.appendChild( renderer.domElement );
-
+  
+  // vr effect
   effect = new THREE.VREffect(renderer);
   effect.setSize(window.innerWidth, window.innerHeight);
+  
+  // scene + camera
   scene  = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(70, wid/hei, 0.1, 1000);
-
+  
+  // vr controls for camera
   controls = new THREE.VRControls( camera );
   controls.standing = true;
   controls.update();
   camera.position.y = controls.userHeight;
 
+  // creating second camera to hold vr camera like a dolly
   dollyCam = new THREE.PerspectiveCamera();
   //dollyCam.position.set(0,20,-50);
-
+  
+  // controls for dollycam
   orbitControls = new THREE.OrbitControls(dollyCam);
   dollyCam.add(camera);
   scene.add(dollyCam);
 
+  // adding crosshair target to camera (making it a child)
   camera.add( crosshair );
+  // adding camera to scene
   scene.add(camera);
-
+  // raycaster initialization
   arrow = new THREE.ArrowHelper( raycaster.ray.direction, (raycaster.ray.origin), 100, 0xffffff );
-  //scene.add( arrow );
 
   // Initialize (Web)VR
   renderer.vr.enabled = true;
   setupVRStage();
 
-  // EVENTS
+  // events
   window.addEventListener('resize', onWindowResize, true );
   window.addEventListener('vrdisplaypresentchange', onWindowResize, true);
 }
@@ -110,19 +117,22 @@ function setupVRStage() {
         setStageDimensions(vrDisplay.stageParameters);
       }
     controls.update();
-
       // setting up button and onclick function programmed in WEBvr to turn page into VR experience and split screen
       vrButton = WEBVR.getButton( vrDisplay, renderer.domElement );
       document.getElementById('vr_bb').appendChild( vrButton );
   } else {
       console.log("NO VR DISPLAYS PRESENT");
   }
+    
+  // sets up physics environment
   initPhysics();
-    createEnvironment();
+  // setting up elements in environment
+  createEnvironment();
   update();
   });
 }
-//Physics configuration
+
+// physics configuration
 function initPhysics() {
   collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
   dispatcher = new Ammo.btCollisionDispatcher( collisionConfiguration );
@@ -138,6 +148,7 @@ function setStageDimensions(stage){
 
 let boxMesh;
 
+// elements in scene
 function createEnvironment() {
   // Lights
   scene.add( new THREE.AmbientLight( 0xffffff, 0.3) );
@@ -153,17 +164,17 @@ function createEnvironment() {
   light2.position.set( 10, 40, - 20 );
   light2.castShadow = true;
   light2.shadow.camera.zoom = 4;
-  //scene.add( light2 );
   light2.target.position.set( 6 , 5.5 , -6 );
-  //scene.add( light2.target );
+  
+  // other elements
   sky();
   roomWalls();
   glassWalls();
   target();
   memories();
-  //song();
 }
-// PLANE OBJECT
+
+// plane object (with rigid body)
 function Plane () {
   this.display = function (material,wid,hei,x,y,z,rotX,rotY) {
     let plane = new THREE.PlaneGeometry( wid, hei, 24 ,24);
@@ -191,10 +202,10 @@ function Plane () {
     physicsWorld.addRigidBody( body );
 
     return planeMesh;
-
   }
 }
-// ROOM WALLS
+
+// room walls setup
 function roomWalls () {
   let transPlane = new THREE.MeshPhongMaterial({
     color: 0x3572B0,
@@ -232,7 +243,8 @@ function roomWalls () {
   let AbovePlane = new Plane();
   scene.add(AbovePlane.display(transPlane,12,12,0,5.5,0, - Math.PI / 2, 0));
 }
-// TARGET
+
+// crosshair target attached to camera setup
 function target () {
   let targetMaterial = new THREE.LineBasicMaterial({ color: 0xAAFFAA });
   let x = 0.01, y = 0.01;
@@ -260,7 +272,8 @@ function target () {
   crosshair.position.z = -0.3;
   camera.add( crosshair );
 }
-//SKYDOME
+
+// skydome
 function sky() {
   let loader = new THREE.TextureLoader();
   let skyGeo = new THREE.SphereGeometry(500, 25, 25);
@@ -271,7 +284,8 @@ function sky() {
   skyDome.material.side = THREE.BackSide;
   scene.add(skyDome);
 }
-//GLASS WALLS
+
+// glass walls
 function glassWalls(){
   frontRefractor = new THREE.Refractor( 10, 6, {
     color: 0x999999,
@@ -337,7 +351,7 @@ function glassWalls(){
     update();
   } );
   dudvMap.wrapS = dudvMap.wrapT = THREE.RepeatWrapping;
-
+  // attaching texture to glass walls
   frontRefractor.material.uniforms.tDudv.value = dudvMap;
   leftRefractor.material.uniforms.tDudv.value = dudvMap;
   rightRefractor.material.uniforms.tDudv.value = dudvMap;
@@ -345,7 +359,8 @@ function glassWalls(){
   backRefractor.material.uniforms.tDudv.value = dudvMap;
   topRefractor.material.uniforms.tDudv.value = dudvMap;
 }
-//MEMORY SPHERES
+
+// memory spheres
 function memories(){
   var geometry2 = new THREE.SphereGeometry( 0.5, 24, 24 );
 
@@ -371,7 +386,7 @@ function memories(){
     let object = THREE.SceneUtils.createMultiMaterialObject( geometry2, materials );
     object.position.set(THREE.Math.randInt(-3,3),THREE.Math.randInt(-2,3),THREE.Math.randInt(-3,0));
 
-    // APPLYING RIGID BODIES
+    // applying rigid bodies
     let shape = new Ammo.btSphereShape( 0.5 );
     shape.setMargin( margin );
     let mass = 0.5 * 2;
@@ -401,7 +416,7 @@ function memories(){
     spheres.push(object.children[1]);
   }
 
-  //CAMERA SPHERE
+  // applying sphere shape and rigid body to camera itself so nothing collides with it
   var origin = new THREE.SphereGeometry( 0.5, 24, 24 );
   let objectSize = 3;
   let margin = 10;
@@ -414,7 +429,7 @@ function memories(){
   cameraSphere.position.z = camera.position.z;
   // mesh.visible = false;
 
-  // APPLYING RIGID BODIES
+  // applying rigid body
   let shape = new Ammo.btSphereShape( 0.6 );
   shape.setMargin( margin );
 
@@ -430,144 +445,32 @@ function memories(){
   let body = new Ammo.btRigidBody( rbInfo );
   body.setFriction( 200 );
   cameraSphere.userData.physicsBody = body;
-
+  
+  // adding it to the scene
   scene.add( cameraSphere );
   dynamicObjects.push( cameraSphere );
   physicsWorld.addRigidBody( body );
 }
 
-// function song() {
-//   var audioListener = new THREE.AudioListener();
-//   var mainSong = new THREE.Audio( audioListener );
-//   camera.add( audioListener );
-//   scene.add( mainSong );
-//
-//   var loader = new THREE.AudioLoader();
-//   loader.load('song/Rain.mp3',
-//     function ( audioBuffer ) {
-//       mainSong.setBuffer( audioBuffer );
-//       mainSong.play();
-//     },
-//     function ( xhr ) {
-//       console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-//     },
-//     function ( xhr ) {
-//       console.log( 'An error happened' );
-//     });
-// }
-window.addEventListener( 'vr controller connected', function( event ){
-  //  Here it is, your VR controller instance.
-  //  It’s really a THREE.Object3D so you can just add it to your scene:
-  var controller = event.detail
-  scene.add( controller )
-  //  HEY HEY HEY! This is important. You need to make sure you do this.
-  //  For standing experiences (not seated) we need to set the standingMatrix
-  //  otherwise you’ll wonder why your controller appears on the floor
-  //  instead of in your hands! And for seated experiences this will have no
-  //  effect, so safe to do either way:
-  controller.standingMatrix = renderer.vr.getStandingMatrix()
-  //  And for 3DOF (seated) controllers you need to set the controller.head
-  //  to reference your camera. That way we can make an educated guess where
-  //  your hand ought to appear based on the camera’s rotation.
-  controller.head = window.camera
-  //  Right now your controller has no visual.
-  //  It’s just an empty THREE.Object3D.
-  //  Let’s fix that!
-  var
-  meshColorOff = 0xDB3236,//  Red.
-  meshColorOn  = 0xF4C20D,//  Yellow.
-  controllerMaterial = new THREE.MeshStandardMaterial({
-    color: meshColorOff
-  }),
-  controllerMesh = new THREE.Mesh(
-    new THREE.CylinderGeometry( 0.005, 0.05, 0.1, 6 ),
-    controllerMaterial
-  ),
-  handleMesh = new THREE.Mesh(
-    new THREE.BoxGeometry( 0.03, 0.1, 0.03 ),
-    controllerMaterial
-  )
-  controllerMaterial.flatShading = true
-  controllerMesh.rotation.x = -Math.PI / 2
-  handleMesh.position.y = -0.05
-  controllerMesh.add( handleMesh )
-  controller.userData.mesh = controllerMesh//  So we can change the color later.
-  controller.add( controllerMesh )
-
-  //  Button events. How easy is this?!
-  //  We’ll just use the “primary” button -- whatever that might be ;)
-  //  Check out the THREE.VRController.supported{} object to see
-  //  all the named buttons we’ve already mapped for you!
-  controller.addEventListener( 'primary press began', function( event ){
-    event.target.userData.mesh.material.color.setHex( meshColorOn )
-    //guiInputHelper.pressed( true );
-    console.log("controller is pressed");
-
-    // check if you're clicking a ball
-    if(intersects.length > 0){
-      INTERSECTED = intersects[ 0 ].object;
-      if(isBallClicked){	// if a ball IS attached, SEND TO BACK
-        camera.remove(INTERSECTED);
-        scene.add(INTERSECTED);
-
-        INTERSECTED.position.x = Math.random() * 4 - 2;
-        INTERSECTED.position.y = Math.random() * 4 - 2;
-        INTERSECTED.position.z = Math.random() * 4 - 2;
-
-        // let PercentX = 50;
-        // let PercentY = 50;
-        // let PositionX = (PercentX / 100) * 2 - 1;
-        // let PositionY = (PercentY / 100) * 2 - 1;
-
-        // INTERSECTED.position.x = PositionX * camera.aspect;
-        // INTERSECTED.position.y = PositionY;
-
-        // INTERSECTED.position.z = Math.random() * 4;
-        // INTERSECTED.rotation.y = Math.random() * Math.PI;
-      } else if(!isBallClicked) { // if there are no attached balls, ATTACH IT
-        INTERSECTED = intersects[ 0 ].object;
-        //INTERSECTED.remove(INTERSECTED.userData.physicsBody);
-        scene.remove(INTERSECTED);
-        camera.add(INTERSECTED);
-
-        let PercentX = 50;
-        let PercentY = 50;
-        let PositionX = (PercentX / 100) * 2 - 1;
-        let PositionY = (PercentY / 100) * 2 - 1;
-
-        INTERSECTED.position.x = PositionX * camera.aspect;
-        INTERSECTED.position.y = PositionY;
-
-        INTERSECTED.position.z = -1;
-        //INTERSECTED.rotation.y += 0.01;
-      }
-      isBallClicked = !isBallClicked;
-    }
-  })
-  controller.addEventListener( 'primary press ended', function( event ){
-    event.target.userData.mesh.material.color.setHex( meshColorOff )
-    console.log("controller has ended");
-  })
-
-  //  Daddy, what happens when we die?
-  controller.addEventListener( 'disconnected', function( event ){
-    controller.parent.remove( controller )
-  })})
-
+// resizing three.js scene based on window width and height
 function onWindowResize(){
-let wid = window.innerWidth;
+  let wid = window.innerWidth;
   let hei = window.innerHeight;
   effect.setSize(wid, hei);
   renderer.setSize(wid, hei);
-camera.aspect = wid/hei;
+  camera.aspect = wid/hei;
   camera.updateProjectionMatrix();
   console.log("UPDATE SIZE");
 }
+
+// getting mouse positions 
 function onMouseMove( event ) {
-mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
-function update(){
+
+// updating elements per frame
+function update() {
   window.requestAnimationFrame(animate);
 }
 
@@ -575,6 +478,7 @@ let fps = 30;
 
 function animate(timestamp) {
   var deltaTime = clock.getDelta();
+  // updating shader uniform values 
   frontRefractor.material.uniforms.time.value += deltaTime;
   leftRefractor.material.uniforms.time.value += deltaTime;
   rightRefractor.material.uniforms.time.value += deltaTime;
@@ -587,11 +491,11 @@ function animate(timestamp) {
   // update the raycaster
   raycaster.set(camera.getWorldPosition(), camera.getWorldDirection());
 
-  // intersect with all scene meshes.
+  // intersect arrow with all scene meshes
   intersects = raycaster.intersectObjects(spheres);
 
   if ( intersects.length > 0) {
-    // Ball highlighting
+    // if raycaster intersects, then heighlight memory sphere by making it bigger
     if ( INTERSECTED != intersects[ 0 ].object & isBallClicked == false ) {
       INTERSECTED = intersects[ 0 ].object;
       let sisterKey = INTERSECTED.userData.sister;
@@ -603,6 +507,7 @@ function animate(timestamp) {
       INTERSECTED.scale.set(1.3, 1.3, 1.3);
       INTERSECTED2.scale.set(1.3, 1.3, 1.3);
     }
+    // otherwise return back to original scale
   } else {
     for(let i=0;i<spheres.length;i++){
       spheres[i].scale.set(1, 1, 1);
@@ -610,12 +515,13 @@ function animate(timestamp) {
     INTERSECTED = null;
     INTERSECTED2 = null;
   }
-
+  // updating camera sphere to camera's position
   cameraSphere.position.x = camera.position.x;
   cameraSphere.position.y = camera.position.y;
   cameraSphere.position.z = camera.position.z;
   updatePhysics( deltaTime );
-
+  
+  // update vr displays 
   if(vrDisplay.isPresenting) {
     setTimeout(function(){
         controls.update();
@@ -630,13 +536,13 @@ function animate(timestamp) {
       renderer.render(scene, camera);
       window.requestAnimationFrame(animate);
   }
-  //time += deltaTime;
 }
 
+// updating physics world
 function updatePhysics( deltaTime ) {
   physicsWorld.stepSimulation( deltaTime, 10 );
 
-  // Update objects
+  // Update objects' rigid bodies position and rotation
   for ( let i = 0, il = dynamicObjects.length; i < il; i++ ) {
     if(dynamicObjects[i].type == "Mesh") {
       cameraSphere.position.x = camera.position.x;
@@ -674,10 +580,12 @@ function updatePhysics( deltaTime ) {
     }
   }
 }
+
 window.addEventListener('mousemove', onMouseMove, false );
+// if user clicks on object, then attach material to camera sphere. If user clicks on object and material IS attached, then make the material invisible
 window.addEventListener("click", function(){
   console.log("I am pressed");
-  if(isBallClicked){	// if a ball IS attached, SEND TO BACK
+  if(isBallClicked){	// if a ball IS attached, make material invisible 
     let material = new THREE.MeshPhongMaterial( {
       color: 0xdddddd, specular: 0x009900, shininess: 30, flatShading: true, visible: false
     })
@@ -693,7 +601,7 @@ window.addEventListener("click", function(){
           }
         }
       }
-      let material = INTERSECTED2.material;
+      let material = INTERSECTED2.material; // change material of camera sphere 
       if (material) {
         console.log(material);
         cameraSphere.material = material;
